16aa12c07db988900e0e0642d486d8c003bd5a5c
jslim1995 <j.liiim95@gmail.com>
2024-03-01 00:11:34 +0900

    create branch jinsu

---
+++ 2402/jinsu/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // List<Integer> input = new ArrayList<Integer>();
+        int[] input = {1, 1, 2, 2};
+        Solution s = new Solution();
+        System.out.println(s.solution(input));
+
+        // Solution.solution();
+    }
+}
