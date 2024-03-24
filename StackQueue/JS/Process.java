ff9731b67bc2a6e1a2af1c673d44643a4dbec0b8
jslim1995 <j.liiim95@gmail.com>
2024-03-24 19:48:21 +0900

    스택 큐 폴더 업데이트

---
+++ StackQueue/JS/Process.java
@@ -0,0 +1,12 @@
+package StackQueue.JS;
+
+/**
+ * Process
+ */
+public class Process {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
---
+++ StackQueue/MY/FunctionDev.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FunctionDev
+ * 8 점...
+ */
+public class FunctionDev {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+    
+}
