b05ba7aff6f03b4a1dc43cfdcb6828fc3a5daae6
jslim1995 <j.liiim95@gmail.com>
2024-03-29 15:41:00 +0900

    프로세스 클리어

--- StackQueue/JS/Process.java
+++ StackQueue/JS/Process.java
@@ -1,12 +1,39 @@
 package StackQueue.JS;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Process
+ * 9점...
  */
 public class Process {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        return answer;
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> locations = new LinkedList<>();
+        int num = 0;
+        int count = 1;
+        for (int priority : priorities) {
+            queue.add(priority);
+            locations.add(num++);
+        }
+        Arrays.sort(priorities);
+        while (queue.peek() != null) {
+            if (queue.peek() == priorities[priorities.length - count]) {
+                queue.poll();
+                if (locations.poll().equals(location)) {
+                    break;
+                } else {
+                    count++;
+                }
+            } else {
+                queue.add(queue.poll());
+                locations.add(locations.poll());
+            }
+
+        }
+        return count;
     }
     
 }
